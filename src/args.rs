19e58836ae98f202505f7de598b17166f612dec2
Uzair Aftab <uzaaft@outlook.com>
2023-11-25 14:49:04 +0100

    docs: Fix docs for args

--- src/args.rs
+++ src/args.rs
@@ -4,13 +4,15 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub(crate) struct Args {
-    /// Name of the person to greet
+    /// Stop the service
     #[arg(long)]
     pub stop_service: bool,
 
+    /// Start the service
     #[arg(long)]
     pub start_service: bool,
 
+    /// Restart the service
     #[arg(long)]
     pub restart_service: bool,
 }
