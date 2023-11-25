162d89e55b6e65d650861bb455d965998f9db2dd
Uzair Aftab <uzaaft@outlook.com>
2023-11-25 14:36:22 +0100

    feat: Add services

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,30 @@
 
 mod args;
 mod config;
+mod service;
 
 use config::Config;
 use heimdall::{configure_logger, spawn_command};
 use std::{collections::HashMap, process::Command};
 use tracing::{debug, info};
 
+use clap::Parser;
+
+use anyhow::{anyhow, Result};
 use global_hotkey::{hotkey::HotKey, GlobalHotKeyEvent, GlobalHotKeyManager};
 use winit::event_loop::{ControlFlow, EventLoopBuilder};
 
-fn main() -> Result<(), impl std::error::Error> {
+fn main() -> Result<()> {
     configure_logger();
     let args = args::Args::parse();
+    if args.start_service {
+        return service::start_service().map_err(|e| anyhow!(e));
+    } else if args.stop_service {
+        return service::stop_service().map_err(|e| anyhow!(e));
+    } else if args.restart_service {
+        return service::restart_service().map_err(|e| anyhow!(e));
+    }
+
     debug!("Starting Heimdall");
 
     let event_loop = EventLoopBuilder::new().build()?;
@@ -40,11 +52,13 @@ fn main() -> Result<(), impl std::error::Error> {
 
     let global_hotkey_channel = GlobalHotKeyEvent::receiver();
 
-    event_loop.run(move |_event, _| {
-        if let Ok(event) = global_hotkey_channel.try_recv() {
-            info!("Received hotkey event: {:?}", event);
-            info!("Command: {:?}", key_command_map.get(&event.id));
-            spawn_command!(key_command_map.get(&event.id).unwrap());
-        }
-    })
+    event_loop
+        .run(move |_event, _| {
+            if let Ok(event) = global_hotkey_channel.try_recv() {
+                info!("Received hotkey event: {:?}", event);
+                info!("Command: {:?}", key_command_map.get(&event.id));
+                spawn_command!(key_command_map.get(&event.id).unwrap());
+            }
+        })
+        .map_err(|e| anyhow!(e))
 }
