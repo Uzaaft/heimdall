4b6c144fe52e4a2be942685899daa7dafd46f6e3
Uzair Aftab <muaf@nmbu.no>
2025-02-20 14:35:09 +0100

    feat: Use vendored in lib, update winit, and refactor code to the new way of doing things

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,60 @@ use tracing::{error, info, trace};
 
 use anyhow::{bail, Result};
 use global_hotkey::{hotkey::HotKey, GlobalHotKeyEvent, GlobalHotKeyManager};
-use winit::event_loop::{ControlFlow, EventLoop};
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::WindowId,
+};
+
+#[derive(Debug)]
+enum AppEvent {
+    HotKey(GlobalHotKeyEvent),
+}
+
+struct App {
+    /// We will need this when we implement reload
+    hotkeys_manager: GlobalHotKeyManager,
+    key_command_map: HashMap<u32, String>,
+}
+
+impl ApplicationHandler<AppEvent> for App {
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
+
+    fn window_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        _event: WindowEvent,
+    ) {
+        unimplemented!();
+    }
+
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, event: AppEvent) {
+        match event {
+            AppEvent::HotKey(event) => {
+                println!("{event:?}");
+                trace!("Received hotkey event: {:?}", event);
+                if global_hotkey::HotKeyState::Released == event.state {
+                    info!("key: {:?} released", &self.key_command_map.get(&event.id));
+                    spawn_command(self.key_command_map.get(&event.id).unwrap()).unwrap();
+                }
+            }
+        }
+    }
+}
 
 fn main() -> Result<()> {
     configure_logger();
 
     info!("Starting Heimdall");
-    info!("Aquiring lock file");
     let file = File::create("/tmp/heim.lock")?;
     if file.try_lock_exclusive().is_err() {
         error!("Couldn't aquire lock-file. Aborting..");
         bail!("Couldn't aquire lock-file. Aborting..");
     }
     info!("Lock file aquired");
-
-    let event_loop = EventLoop::new()?;
-    event_loop.set_control_flow(ControlFlow::Wait);
 
     let hotkeys_manager = GlobalHotKeyManager::new()?;
 
@@ -45,19 +83,22 @@ fn main() -> Result<()> {
         })
         .collect();
 
-    let global_hotkey_channel = GlobalHotKeyEvent::receiver();
+    let mut app = App {
+        hotkeys_manager,
+        key_command_map,
+    };
 
-    event_loop.run(move |_event, _| {
-        if let Ok(event) = global_hotkey_channel.try_recv() {
-            trace!("Received hotkey event: {:?}", event);
-            if global_hotkey::HotKeyState::Released == event.state {
-                info!("key: {:?} released", key_command_map.get(&event.id));
-                spawn_command(key_command_map.get(&event.id).unwrap());
-            }
-        }
-    })?;
+    let event_loop = EventLoop::<AppEvent>::with_user_event().build()?;
+    event_loop.set_control_flow(ControlFlow::Wait);
+    let proxy = event_loop.create_proxy();
 
-    file.unlock()?;
+    GlobalHotKeyEvent::set_event_handler(Some(move |event| {
+        let _ = proxy.send_event(AppEvent::HotKey(event));
+    }));
+
+    event_loop.run_app(&mut app).unwrap();
+
+    fs4::fs_std::FileExt::unlock(&file)?;
     // Remove file
     Ok(std::fs::remove_file("/tmp/heim.lock")?)
 }
